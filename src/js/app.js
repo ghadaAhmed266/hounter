2fca349526437b3605f77f674331df3122381fea
moustafaamindev <dev.moustafa.amin@gmail.com>
2023-03-09 00:30:21 +0200

    add owl carusel

--- src/js/app.js
+++ src/js/app.js
@@ -4,20 +4,3 @@ import '../scss/app.scss';
 
 /* Demo JS */
 import './demo.js';
-
-import './jquery.min.js';
-import './owl.carousel.min.js';
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
-  $('.loop').owlCarousel({
-    center: true,
-    items:2,
-    loop:true,
-    margin:10,
-    responsive:{
-        600:{
-            items:4
-        }
-    }
-});
